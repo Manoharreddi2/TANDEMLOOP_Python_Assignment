bfb26d866a346d5583109e97a7292b32c9f15cae
Manoharreddi2 <166891860+Manoharreddi2@users.noreply.github.com>
2025-09-24 22:51:14 +0530

    Program1.py

---
+++ Program1.py
@@ -0,0 +1,27 @@
+class Calculator:
+    def __init__(self, a, b, operation):
+        self.a = a
+        self.b = b
+        self.operation = operation.lower() 
+
+    def calculate(self):
+        if self.operation == "add":
+            return self.a + self.b
+        elif self.operation == "subtract":
+            return self.a - self.b
+        elif self.opesration == "multiply":
+            return self.a * self.b
+        elif self.operation == "divide":
+            if self.b != 0:
+                return self.a / self.b
+            else:
+                return "Error: Division by zero"
+        else:
+            return "Invalid operation"
+
+
+a=float(input("Enter the first number: "))
+b=float(input("Enter the second number: "))
+op=str(input("Enter the Operation: "))
+calc = Calculator(a, b, op)
+print("Result:", calc.calculate())
