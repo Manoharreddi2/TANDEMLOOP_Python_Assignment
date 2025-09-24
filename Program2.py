871c4561fb2e5a26319345e3df7e7f0cf5c1ab6c
Manoharreddi2 <166891860+Manoharreddi2@users.noreply.github.com>
2025-09-24 22:52:16 +0530

    Program2.py

---
+++ Program2.py
@@ -0,0 +1,9 @@
+def generate_series(x):
+    num=1
+    for i in range(x):
+        print(num,end=" ")
+        num=num+2
+
+# For Example 
+a = int(input("Enter the Integer : "))
+generate_series(a)
